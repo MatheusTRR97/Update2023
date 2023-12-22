5849c94fb407ecd0e39c38889466bf0e37bc5a1e
Matheus Trindade <matheustrr@outlook.com.br>
2023-12-22 12:59:36 -0300

    Atualizações de planilhas para adicionar vdd's

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
-    { 
+    {
         private readonly SellerService _sellerService;
-    
+
         public SellersController(SellerService sellerService)
         {
             _sellerService = sellerService;
@@ -19,6 +20,18 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
